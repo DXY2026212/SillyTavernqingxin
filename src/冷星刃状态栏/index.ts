9e38a80cdcf0f26250fa4762923c3d673e659d1d
倾心 <261173754+DXY2026212@users.noreply.github.com>
2026-03-03 18:39:57 +0800

    更新

--- src/冷星刃状态栏/index.ts
+++ src/冷星刃状态栏/index.ts
@@ -1,17 +1,2 @@
 import './加载和卸载时执行函数';
-import 设置界面 from './设置界面.vue';
-
-$(() => {
-  const app = createApp(设置界面).use(createPinia());
-
-  const $app = createScriptIdDiv().appendTo('#extensions_settings2');
-  app.mount($app[0]);
-
-  const { destroy } = teleportStyle();
-
-  $(window).on('pagehide', () => {
-    app.unmount();
-    $app.remove();
-    destroy();
-  });
-});
+import './界面.vue';
